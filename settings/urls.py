abd7d9f8b6675236ca0d4e469cdb74ee81709644
WinterOdin <cczuryszkiewicz@gmail.com>
2021-03-20 13:15:09 +0100

    react added

--- settings/urls.py
+++ settings/urls.py
@@ -6,7 +6,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('api.urls')),
+    path('api/',include('api.urls')),
+    path('',include('frontend.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
