4589eee2404d97be2046fb5ce02761add08c775d
WinterOdin <cczuryszkiewicz@gmail.com>
2021-04-12 17:57:25 +0200

    som backend and frontend

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('room', RoomView.as_view()),
     path('create', CreateRoom.as_view(),),
     path('get-info', GetRoom.as_view(),)
+    path('join-room', JoinRoom.as_view())
 ]
--- frontend/src/components/CreateRoomPage.js
+++ frontend/src/components/CreateRoomPage.js
@@ -47,10 +47,9 @@ export default class CreateRoomPage extends Component {
       }),
     };
     fetch("/api/create", requestOptions)
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-  }
-
+    .then((response) => response.json())
+    .then((data) => this.props.history.push("/room/" + data.code));
+}
   render() {
     return (
       <Grid container spacing={1}>
